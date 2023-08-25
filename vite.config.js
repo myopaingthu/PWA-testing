a6514cc00adbbbc32f3a5476b89e5a66c83ea4c9
Myo Paing Thu <myopaingthu@novaseventech.com>
2023-08-25 13:49:40 +0630

    [master] icon image

--- vite.config.js
+++ vite.config.js
@@ -25,22 +25,22 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: 'favicon.ico',
+            src: 'img/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'favicon.ico',
+            src: 'img/icons/icon-256x256.png',
             sizes: '256x256',
             type: 'image/png'
           },
           {
-            src: 'favicon.ico',
+            src: 'img/icons/icon-384x384.png',
             sizes: '384x384',
             type: 'image/png'
           },
           {
-            src: 'favicon.ico',
+            src: 'img/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
